9f81f36c0aa9ae49788e8154c3f0a7295feae4cf
markusanttilasc <markus.anttila@dainstudios.com>
2025-12-02 21:00:14 +0200

    Fix hydration errors by adding 'use client' directive to page components

--- matchnord-app/src/app/[locale]/about/page.tsx
+++ matchnord-app/src/app/[locale]/about/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Trophy, Users, Globe, Zap, Smartphone, BarChart3 } from "lucide-react";
 import { useTranslations } from 'next-intl';
