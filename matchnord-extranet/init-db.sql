0bf60204decc29004825ac9ed55d7fc4cf1701f5
markusanttilasc <markus.anttila@dainstudios.com>
2025-12-20 10:16:06 +0200

    remove dead code

--- matchnord-extranet/init-db.sql
+++
@@ -1,9 +0,0 @@
--- Initialize the database with any custom settings
--- This file runs when the PostgreSQL container starts for the first time
-
--- Enable some useful extensions
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "citext";
-
--- Set timezone to UTC
-SET timezone = 'UTC';
